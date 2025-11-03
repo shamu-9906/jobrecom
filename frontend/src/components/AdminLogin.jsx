a09d6eda92df5abae07bd3f4024e5ff0e9639669
shamu-9906 <thisissyamala2006@gmail.com>
2025-11-03 23:31:02 +0530

    Update AdminLogin.jsx

--- frontend/src/components/AdminLogin.jsx
+++ frontend/src/components/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Admin.css";
+import "./AdminLogin.css";
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
